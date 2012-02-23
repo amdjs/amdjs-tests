cf29f92506d915a998b5b17f6d7c6ce242e7b9d6
Taka Kojima <taka.kojima@ff0000.com>
2012-02-23 15:42:37 -0800

    Adding Needs.JS

---
+++ impl/needs/config.js
@@ -0,0 +1,13 @@
+var go = require,
+	config = require,
+	implemented = {
+		basic: true,
+		anon: true,
+		require: true,
+		funcString: true,
+		namedWrapped: true,
+		plugins: false,
+		pluginDynamic: false
+	};
+
+require = undefined;
