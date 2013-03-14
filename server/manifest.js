ff21907069ae7b459a76751782af227d8a0b2223
Jakob Heuser <jakob@felocity.com>
2013-03-14 11:17:25 -0700

    simplified manifest for commenting, uncommenting, and adding

--- server/manifest.js
+++ server/manifest.js
@@ -1,23 +1,27 @@
 // implied impl/ directory...
-exports.manifest = {
-  "requirejs": {
-    "name":   'RequireJS @ 2.1.5',
-    "impl":   'requirejs/require.js',
-    "config": 'requirejs/config.js'
-  },
-  "needs": {
-    "name":   'Needs JS @ a915dce3',
-    "impl":   'needs/needs.js',
-    "config": 'needs/config.js'
-  },
-  "inject": {
-    "name":   'Inject @ 0.4.1',
-    "impl":   'inject/inject.js',
-    "config": 'inject/config.js'
-  },
-  "lsjs": {
-    "name":   'lsjs @ e61412da',
-    "impl":   'lsjs/lsjs.js',
-    "config": 'lsjs/config.js'
-  }
+exports.manifest = {};
+
+exports.manifest.requirejs = {
+  name:   'RequireJS @ 2.1.5',
+  impl:   'requirejs/require.js',
+  config: 'requirejs/config.js'
 };
+
+exports.manifest.needs = {
+  name:   'Needs JS @ a915dce3',
+  impl:   'needs/needs.js',
+  config: 'needs/config.js'
+};
+
+exports.manifest.inject = {
+  name:   'Inject @ 0.4.1',
+  impl:   'inject/inject.js',
+  config: 'inject/config.js'
+};
+
+// disabled. Circular loop resolution issue
+// exports.manifest.lsjs = {
+//   name:   'lsjs @ e61412da',
+//   impl:   'lsjs/lsjs.js',
+//   config: 'lsjs/config.js'
+// };
