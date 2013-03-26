e4afdb59e87ef07cb503722546b090bc7fb5a702
Jakob Heuser <jakob@felocity.com>
2013-03-26 16:19:08 -0700

    Resolve Issue 13 anon_relative using config_path The original anon_relative was using logic required by config_path in order to properly test the relative path information. To remedy, the anon_relative has the path information removed, and a new test config_path_relative was created as part of the configPaths suite. This test ensures that relative modules remain relative to their module ID and not to their URL, even in the event of a path remap.
    Github ID: #13

--- tests/anon_relative/impl/array.js
+++ tests/anon_relative/impl/array.js
@@ -1,6 +1,7 @@
-define(['./util'], function (util) {
+define(['./util', 'util'], function (dotUtil, util) {
     return {
         name: 'impl/array',
+        dotUtilName: dotUtil.name,
         utilName: util.name
     };
 });
